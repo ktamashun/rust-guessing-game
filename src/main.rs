d447d02acfad7d584f34ddd8a67b682b4414e358
ktamashun <kovacs.tamas.hun@gmail.com>
2017-01-28 14:07:53 +0100

    Adding loop and break.

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,26 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please inpiut your guess.");
+    loop {
+        println!("Please inpiut your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("Yout guessed: {}", guess);
+        println!("Yout guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less      => println!("Too small!"),
-        Ordering::Greater   => println!("Too large!"),
-        Ordering::Equal     => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less      => println!("Too small!"),
+            Ordering::Greater   => println!("Too large!"),
+            Ordering::Equal     => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
